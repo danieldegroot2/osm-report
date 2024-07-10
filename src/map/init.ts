70c230b3aa15705a2d4dbc0f57d423e3865735f6
danieldegroot2 <67521919+danieldegroot2@users.noreply.github.com>
2024-07-10 16:28:39 +0200

    Update openstreetmap layer in init.ts
    See https://github.com/openstreetmap/operations/issues/737

--- src/map/init.ts
+++ src/map/init.ts
@@ -29,7 +29,7 @@ export default function (container: HTMLElement): Map {
   }).addTo(map);
 
   const baseLayer = new TileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
     {
       attribution:
         "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>"
